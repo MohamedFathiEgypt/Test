60dbe190d7730a558ad427c8707485cff1aefab7
Mohamed Fathi <mohamed.othman@arabyads.com>
2025-11-22 16:13:40 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pandas_ta_fork",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy==1.26.4",
+        # add any other dependencies here
+    ],
+)
