74fa6fde3c9a4a7744643aeeb6c2105cfc84492d
Mohamed Fathi <mohamed.othman@arabyads.com>
2025-11-22 16:21:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pandas_ta_fork",
-    version="0.0.1",
+    name="pandas-ta",
+    version="0.1",
     packages=find_packages(),
-    install_requires=[
-        "numpy==1.26.4",
-        # add any other dependencies here
-    ],
+    install_requires=[],
+    # other metadata
 )
